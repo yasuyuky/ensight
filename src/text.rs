99b96149db5685f336570e003c89bdba654aad6e
yasuyuky <yasuyuki.ymd@gmail.com>
2022-09-17 16:03:27 +0900

    Use colored

--- src/text.rs
+++ src/text.rs
@@ -2,6 +2,7 @@ use crate::get;
 use crate::insight::{InsightItem, Insights, Item, Items};
 use crate::vcs::Vcs;
 use ansi_term::Colour;
+use colored::{Color, Colorize};
 
 pub async fn print_all(vcs: &Vcs, slug: &str, sort: bool, n: Option<usize>) -> anyhow::Result<()> {
     let path = format!("insights/{}/{}/workflows", &vcs, &slug);
